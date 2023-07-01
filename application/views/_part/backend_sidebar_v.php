ccc5727e3831229b53c2c3debb176c28d1ecb5d5
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-07-01 14:27:57 +0700

    change name

--- application/views/_part/backend_sidebar_v.php
+++ application/views/_part/backend_sidebar_v.php
@@ -6,7 +6,7 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-      <div class="sidebar-brand-text mx-3">PT Nippisun Indonesia</div>
+      <div class="sidebar-brand-text mx-3">PT Nippisun</div>
     </a>
 
     <!-- Divider -->
