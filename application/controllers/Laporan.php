0b87e95b88c58b1bede882952a3be8c69990883e
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-07-05 17:21:58 +0700

    revisi

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -47,9 +47,9 @@ class Laporan extends CI_Controller
           }
         } else {
           if ($from == $to) {
-            $data['data'] = $this->db->query("SELECT * from barang where kategori = " . $post['kategori'] . " and tanggal = '$from'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from barang where kategori = '" . $post['kategori'] . "' and tanggal = '$from'")->result_array();
           } else {
-            $data['data'] = $this->db->query("SELECT * from barang where kategori = " . $post['kategori'] . " and tanggal between '$from' and '$to'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from barang where kategori = '" . $post['kategori'] . "' and tanggal between '$from' and '$to'")->result_array();
           }
         }
       }
@@ -63,7 +63,7 @@ class Laporan extends CI_Controller
         if ($post['kategori'] == "semua") {
           $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang")->result_array();
         } else {
-          $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where kategori = '" . $post['kategori'] . "'")->result_array();
+          $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where barang.kategori = '" . $post['kategori'] . "'")->result_array();
         }
       } else {
         $from = date('Y-m-d', strtotime($post['tgl_mulai']));
@@ -72,20 +72,21 @@ class Laporan extends CI_Controller
         $data['tgl_akhir'] = $post['tgl_akhir'];
         if ($post['kategori'] == "semua") {
           if ($from == $to) {
-            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where tanggal = '$from'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where persediaan.tanggal = '$from'")->result_array();
           } else {
-            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where tanggal between '$from' and '$to'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where persediaan.tanggal between '$from' and '$to'")->result_array();
           }
         } else {
           if ($from == $to) {
-            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where kategori = " . $post['kategori'] . " and tanggal = '$from'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where barang.kategori = '" . $post['kategori'] . "' and persediaan.tanggal = '$from'")->result_array();
           } else {
-            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where kategori = " . $post['kategori'] . " and tanggal between '$from' and '$to'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from persediaan inner join barang on persediaan.id_barang=barang.id_barang where barang.kategori = '" . $post['kategori'] . "' and persediaan.tanggal between '$from' and '$to'")->result_array();
           }
         }
       }
       $data['title'] = 'Laporan Daftar Persediaan Barang';
       $this->load->library('pdf');
+      // $this->load->view('laporan/laporan_persediaan_barang', $data);
       $this->pdf->load_view('laporan/laporan_persediaan_barang', $data);
     }
 
@@ -109,14 +110,15 @@ class Laporan extends CI_Controller
           }
         } else {
           if ($from == $to) {
-            $data['data'] = $this->db->query("SELECT * from tr_masuk inner join barang on tr_masuk.id_barang=barang.id_barang inner join barang_masuk on tr_masuk.id_bmasuk=barang_masuk.id_bmasuk inner join pemasok on tr_masuk.id_pemasok=pemasok.id_pemasok where barang.kategori = " . $post['kategori'] . " and barang_masuk.tanggal = '$from'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from tr_masuk inner join barang on tr_masuk.id_barang=barang.id_barang inner join barang_masuk on tr_masuk.id_bmasuk=barang_masuk.id_bmasuk inner join pemasok on tr_masuk.id_pemasok=pemasok.id_pemasok where barang.kategori = '" . $post['kategori'] . "' and barang_masuk.tanggal = '$from'")->result_array();
           } else {
-            $data['data'] = $this->db->query("SELECT * from tr_masuk inner join barang on tr_masuk.id_barang=barang.id_barang inner join barang_masuk on tr_masuk.id_bmasuk=barang_masuk.id_bmasuk inner join pemasok on tr_masuk.id_pemasok=pemasok.id_pemasok where barang.kategori = " . $post['kategori'] . " and barang_masuk.tanggal between '$from' and '$to'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from tr_masuk inner join barang on tr_masuk.id_barang=barang.id_barang inner join barang_masuk on tr_masuk.id_bmasuk=barang_masuk.id_bmasuk inner join pemasok on tr_masuk.id_pemasok=pemasok.id_pemasok where barang.kategori = '" . $post['kategori'] . "' and barang_masuk.tanggal between '$from' and '$to'")->result_array();
           }
         }
       }
       $data['title'] = 'Laporan Daftar Barang Masuk';
-      $this->load->view('laporan/laporan_barang_masuk', $data);
+      $this->load->library('pdf');
+      $this->pdf->load_view('laporan/laporan_barang_masuk', $data);
     }
 
     if ($post['jenis_laporan'] == "Barang Keluar") {
@@ -139,14 +141,15 @@ class Laporan extends CI_Controller
           }
         } else {
           if ($from == $to) {
-            $data['data'] = $this->db->query("SELECT * from tr_keluar inner join barang on tr_keluar.id_barang=barang.id_barang inner join barang_keluar on tr_keluar.id_bkeluar=barang_keluar.id_bkeluar inner join pelanggan on tr_keluar.id_pelanggan=pelanggan.id_pelanggan where barang.kategori = " . $post['kategori'] . " and barang_keluar.tanggal = '$from'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from tr_keluar inner join barang on tr_keluar.id_barang=barang.id_barang inner join barang_keluar on tr_keluar.id_bkeluar=barang_keluar.id_bkeluar inner join pelanggan on tr_keluar.id_pelanggan=pelanggan.id_pelanggan where barang.kategori = '" . $post['kategori'] . "' and barang_keluar.tanggal = '$from'")->result_array();
           } else {
-            $data['data'] = $this->db->query("SELECT * from tr_keluar inner join barang on tr_keluar.id_barang=barang.id_barang inner join barang_keluar on tr_keluar.id_bkeluar=barang_keluar.id_bkeluar inner join pelanggan on tr_keluar.id_pelanggan=pelanggan.id_pelanggan where barang.kategori = " . $post['kategori'] . " and barang_keluar.tanggal between '$from' and '$to'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from tr_keluar inner join barang on tr_keluar.id_barang=barang.id_barang inner join barang_keluar on tr_keluar.id_bkeluar=barang_keluar.id_bkeluar inner join pelanggan on tr_keluar.id_pelanggan=pelanggan.id_pelanggan where barang.kategori = '" . $post['kategori'] . "' and barang_keluar.tanggal between '$from' and '$to'")->result_array();
           }
         }
       }
       $data['title'] = 'Laporan Daftar Barang Keluar';
-      $this->load->view('laporan/laporan_barang_keluar', $data);
+      $this->load->library('pdf');
+      $this->pdf->load_view('laporan/laporan_barang_keluar', $data);
     }
 
     if ($post['jenis_laporan'] == "Permintaan Barang") {
@@ -169,14 +172,15 @@ class Laporan extends CI_Controller
           }
         } else {
           if ($from == $to) {
-            $data['data'] = $this->db->query("SELECT * from tr_permintaan inner join permintaan_barang on tr_permintaan.id_permintaan=permintaan_barang.id_permintaan inner join barang on barang.id_barang=tr_permintaan.id_barang where barang.kategori = " . $post['kategori'] . " and permintaan_barang.tgl_permintaan = '$from'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from tr_permintaan inner join permintaan_barang on tr_permintaan.id_permintaan=permintaan_barang.id_permintaan inner join barang on barang.id_barang=tr_permintaan.id_barang where barang.kategori = '" . $post['kategori'] . "' and permintaan_barang.tgl_permintaan = '$from'")->result_array();
           } else {
-            $data['data'] = $this->db->query("SELECT * from tr_permintaan inner join permintaan_barang on tr_permintaan.id_permintaan=permintaan_barang.id_permintaan inner join barang on barang.id_barang=tr_permintaan.id_barang where barang.kategori = " . $post['kategori'] . " and permintaan_barang.tgl_permintaan between '$from' and '$to'")->result_array();
+            $data['data'] = $this->db->query("SELECT * from tr_permintaan inner join permintaan_barang on tr_permintaan.id_permintaan=permintaan_barang.id_permintaan inner join barang on barang.id_barang=tr_permintaan.id_barang where barang.kategori = '" . $post['kategori'] . "' and permintaan_barang.tgl_permintaan between '$from' and '$to'")->result_array();
           }
         }
       }
       $data['title'] = 'Laporan Daftar Permintaan Barang';
-      $this->load->view('laporan/laporan_permintaan_barang', $data);
+      $this->load->library('pdf');
+      $this->pdf->load_view('laporan/laporan_permintaan_barang', $data);
     }
   }
 }
