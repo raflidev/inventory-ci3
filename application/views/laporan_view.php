e5af9129f14a3e2777f61dd5d6144a90d78ff063
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-07-06 14:02:34 +0700

    a

--- application/views/laporan_view.php
+++ application/views/laporan_view.php
@@ -85,4 +85,4 @@
 
 <!-- Page level custom scripts -->
 <script src="<?= base_url() ?>assets/backend/js/demo/chart-area-demo.js"></script>
-<script src="<?= base_url() ?>assets/backend/js/demo/chart-pie-demo.js"></script>
+<script src="<?= base_url() ?>assets/backend/js/demo/chart-pie-demo.js"></script>`
